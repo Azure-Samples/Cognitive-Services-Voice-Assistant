e0adfb7ad0347f7358cafde5f68445b3563b0ac4
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2019-12-26 15:59:33 -0800

    Update Nugets

--- samples/clients/csharp-wpf/VoiceAssistantClient/GlobalSuppressions.cs
+++ samples/clients/csharp-wpf/VoiceAssistantClient/GlobalSuppressions.cs
@@ -9,5 +9,6 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1512:Single-line comments should not be followed by blank line", Justification = "Approved")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "Approved")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleType", Justification = "Approved")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1316:TupleElementNamesShouldUseCorrectCasing", Justification = "Approved")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Approved", Scope = "member", Target = "~P:VoiceAssistantClient.SettingsDialog.UrlOverride")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "Approved", Scope = "member", Target = "~P:VoiceAssistantClient.Settings.RuntimeSettings.UrlOverride")]
