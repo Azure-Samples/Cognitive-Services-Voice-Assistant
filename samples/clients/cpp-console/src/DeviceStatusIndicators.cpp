5956241999e2c9ea19dc72fe71f5ca84b93f4683
Christopher Schraer <32145632+chschrae@users.noreply.github.com>
2020-02-24 15:22:57 -0800

    Chschrae/nightfury (#52)
    * initial checkin
    
    * working on nightfury
    
    * added some build scripts and libs compiled with flags.Added compile definition for nightfury
    
    * cleaned up unnecessary files
    
    * Revert "cleaned up unnecessary files"
    
    This reverts commit 223875c2616fbdbeec9110384e8087cfe27b7e72.
    
    * readded source
    
    * updated build scripts and separated nightfury specific code
    
    * removed kws from file and vscode dir
    
    * removed old audio player and updated from file build
    
    * added dockerfile and started a readme
    
    * ignored arm emulator binary
    
    * fixed x64 build script
    
    * updated docker instructions and build to use name instead of ID
    
    * created default build. Added default device status indicators. updated readme
    
    * updated readme
    
    * removed many extra configs
    
    * added copyright tags to files. Added windows project with audioplayer files. Audio player not working yet
    
    * add LINUX definition to scripts and ifdef to json header
    
    * renabled audio
    
    * fixed some warnings and removed some commented code
    
    * more comments removed
    
    * removed keys from config and removed other configs
    
    * added some more to readme
    
    * updated readme with bullets
    
    * added subscription key and region replacement text
    
    * added comments around events
    
    * working on windows audio. No macros left
    
    * windows audio player working
    
    * no warnings
    
    * no longer need the TLS workaround. Suppressed warnings from json.hpp. Enabled code analysis on builds for windows project.
    
    * renamed folder
    
    * removed libs and excluded out dirs
    
    * added cppConsole app to yml file
    
    * added cpp sample to yml file
    
    * fixed a comment
    
    * added sample3Name
    
    * fixed task name
    
    * added nuget restore to sample 3
    
    * fixed include directory for all configurations

---
+++ samples/clients/cpp-console/src/DeviceStatusIndicators.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "DeviceStatusIndicators.h"
+
+
+void DeviceStatusIndicators::SetStatus(const DeviceStatus status){
+    std::cout << "New status : " << DeviceStatusNames::to_string(status) << std::endl;
+    switch (status)
+    {
+    case DeviceStatus::Idle:
+        break;
+    case DeviceStatus::Initializing:
+        break;
+    case DeviceStatus::Ready:
+        break;
+    case DeviceStatus::Detecting:
+        break;
+    case DeviceStatus::Listening:
+        break;
+    case DeviceStatus::Thinking:
+        break;
+    case DeviceStatus::Speaking:
+    default:
+        break;
+    }
+}
