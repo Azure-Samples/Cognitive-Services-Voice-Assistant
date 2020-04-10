3f2fbf530d207fb8859a103a3bdd095048434e9a
Chris Schraer <32145632+chschrae@users.noreply.github.com>
2020-04-10 10:02:06 -0700

    added permission setting for container

--- custom-commands/hospitality/deployment/deployContainerFiles.ps1
+++ custom-commands/hospitality/deployment/deployContainerFiles.ps1
@@ -11,5 +11,5 @@ $storageResourceId = $storageResource.id
 
 az role assignment create --role "Storage Blob Data Owner" --assignee $userName --scope $storageResourceId
     
-az storage container create --account-name $storageName --name $containerName --auth-mode login
+az storage container create --account-name $storageName --name $containerName --public-access container --auth-mode login
 az storage blob upload-batch -d $containerName -s ../storage-files --auth-mode login --account-name $storageName
