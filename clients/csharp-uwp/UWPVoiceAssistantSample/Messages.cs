efa2350f45383286b28bcdcf12da392f79690c60
Sajjad Plumber (INFOSYS LIMITED) <v-saplum@microsoft.com>
2020-04-17 15:14:48 -0700

    testing conversationview

---
+++ clients/csharp-uwp/UWPVoiceAssistantSample/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    public class Messages
+    {
+        public string Body { get; set; }
+        public string Time { get; set; }
+        public bool Sent { get; set; }
+        public bool Received { get; set; }
+    }
+}
