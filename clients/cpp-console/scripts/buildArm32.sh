dccc1081cefd25bb0c021bbb0a9af3192eda2a81
Chris Schraer <32145632+chschrae@users.noreply.github.com>
2020-05-05 14:17:47 -0700

    fixed line endings in sh files. Removed redirection of stdout in run.sh. Updated buildArm32.sh to grab binaries and headers using wget

--- clients/cpp-console/scripts/buildArm32.sh
+++ clients/cpp-console/scripts/buildArm32.sh
@@ -2,6 +2,14 @@
 clear
 cd ..
 mkdir out
+mkdir SpeechSDK
+
+echo "Downloading Speech SDK binaries"
+wget -c https://aka.ms/csspeech/linuxbinary -O - | tar -xz -C ./SpeechSDK
+
+echo "Copying binaries to lib folder and headers to include"
+cp -Rf ./SpeechSDK/SpeechSDK*/* .
+
 echo "Building Raspberry Pi sample"
 g++ -Wno-psabi \
 src/common/mainAudio.cpp \
@@ -9,6 +17,8 @@ src/linux/LinuxAudioPlayer.cpp \
 src/common/AudioPlayerEntry.cpp \
 src/common/AgentConfiguration.cpp \
 src/common/DeviceStatusIndicators.cpp \
+src/common/DialogConnector.cpp \
+src/common/helper.cpp \
 -o ./out/sample.exe \
 -std=c++14 \
 -D LINUX \
@@ -23,3 +33,5 @@ src/common/DeviceStatusIndicators.cpp \
 
 cp ./scripts/run.sh ./out
 chmod +x ./out/run.sh
+
+echo "Done. To start the demo execute:\nRun cd ../out\n./run.sh <path_to_configFile>"
