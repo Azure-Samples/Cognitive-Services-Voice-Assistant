d33dc58ef88f6a2d52e8d0ea5b27be6e21b9ccd3
qfweiwei <22325743+qfweiwei@users.noreply.github.com>
2020-05-13 15:24:48 -0700

    Generalize CC deployment scripts based on current folder structure an… (#367)
    * Generalize CC deployment scripts based on current folder structure and naming convention.
    
    * Update azuredeploy.parameters.json for using generic names

---
+++ custom-commands/scripts/deployAzureFunction.ps1
@@ -0,0 +1,34 @@
+Param(
+    [string] $appName = $(Read-Host -prompt "appName"),
+    [string] $resourceName = $(Read-Host -prompt "resourceName"),
+    [string] $functionName = $(Read-Host -prompt "functionName")
+)
+
+[Console]::ResetColor()
+$ErrorActionPreference = "Stop"
+$resourceGroup = $resourceName
+$publishZip = "publish.zip"
+$publishFolder = "../$appName/azure-function/Virtual$((Get-Culture).TextInfo.ToTitleCase($appName))App/bin/Release/netcoreapp2.1/publish"
+$sln = "../$appName/azure-function/Virtual$((Get-Culture).TextInfo.ToTitleCase($appName))App.sln"
+
+Write-Host "Cleaning and building solution at $sln"
+$output = dotnet clean $sln -c Release
+Write-Verbose "$output"
+$output = dotnet publish $sln -c Release
+Write-Verbose "$output"
+
+# zip up the publish folder
+$absolutePublishFolder = Convert-Path($publishFolder)
+$absoluteCurrentFolder = Convert-Path(".")
+if (Test-path "$publishZip") { Remove-item "$publishZip" }
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::CreateFromDirectory($absolutePublishFolder, "$absoluteCurrentFolder/$publishZip")
+
+Write-Host "Deploying zipped folder at $publishZip"
+# deploy the zipped package
+$output = az functionapp deployment source config-zip -g $resourceGroup -n $functionName --src $publishZip | ConvertFrom-Json
+if (!$output) {
+    Write-Error "Failed to deploy Azure function"
+    Write-Error $output
+    exit
+}
