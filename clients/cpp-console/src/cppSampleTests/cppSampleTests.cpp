63e93029d7b9b9a703ae0c90f0c58281a4d13064
Sajjad Plumber (INFOSYS LIMITED) <v-saplum@microsoft.com>
2020-06-15 13:25:04 -0700

    update with play and stop methods

--- clients/cpp-console/src/cppSampleTests/cppSampleTests.cpp
+++ clients/cpp-console/src/cppSampleTests/cppSampleTests.cpp
@@ -1,10 +1,12 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "WindowsAudioPlayer.h"
+#include "DialogManager.h"
 //#include "AudioPlayer.h"
 //#include "AudioPlayerEntry.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Microsoft::CognitiveServices::Speech;
 
 namespace cppSampleTests
 {
@@ -19,13 +21,43 @@ namespace cppSampleTests
 			Assert::AreEqual(S_OK, hr);
 		}
 
-		TEST_METHOD(TestWindowsAudioPlayerInitializeWithUnsupportedAudioFormat)
+		//TEST_METHOD(TestWindowsAudioPlayerInitializeWithUnsupportedAudioFormat)
+		//{
+		//	AudioPlayer::WindowsAudioPlayer player;
+		//	HRESULT hr = player.Initialize("stereo", IAudioPlayer::AudioPlayerFormat::Stereo48khz16bit);
+		//	Assert::AreEqual(E_FAIL, hr);
+		//}
+
+		TEST_METHOD(TestWindowsAudioPlayerPlay) 
 		{
+			int rc = 0;
+
+			const string& wavFile = "C:\\Users\\v-saplum\\Downloads\\spx-zips\\spx-netcore30-win-x64\\CognitiveServicesVoiceAssistantIntro.wav";
+			//const string& wavFile = "this";
+			fstream fs;
+			fs.open(wavFile, ios_base::binary | ios_base::in);
+			fs.seekg(44);
+
+			std::array<uint8_t, 1000> buffer;
+			fs.read((char*)wavFile.data(), (uint32_t)wavFile.size());
+			
+			fs.close();
+
 			AudioPlayer::WindowsAudioPlayer player;
-			HRESULT hr = player.Initialize("default", IAudioPlayer::AudioPlayerFormat::Stereo48khz16bit);
-			Assert::AreNotEqual(S_OK, hr);
+			player.Initialize();
+			int result = player.Play(buffer.data(), buffer.size());
+			Assert::AreEqual(rc, result);
 		}
 
-		
+		TEST_METHOD(TestWindowsAudioPlayerStop) 
+		{
+			int rc = 0;
+			AudioPlayer::WindowsAudioPlayer player;
+			player.Initialize();
+			TestWindowsAudioPlayerPlay();
+
+			int result = player.Stop();
+			Assert::AreEqual(rc, result);
+		}
 	};
 }
