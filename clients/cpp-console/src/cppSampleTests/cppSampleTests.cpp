93e063170efc8ea458691ab94578f3b160795681
Sajjad Plumber (INFOSYS LIMITED) <v-saplum@microsoft.com>
2020-06-17 14:40:39 -0700

    update tests to reduce repeating code

--- clients/cpp-console/src/cppSampleTests/cppSampleTests.cpp
+++ clients/cpp-console/src/cppSampleTests/cppSampleTests.cpp
@@ -26,42 +26,20 @@ namespace cppSampleTests
         TEST_METHOD(TestWindowsAudioPlayerPlay) 
         {
             int rc = 0;
-            int bytesRead = 0;
-            int result = 1;
-            fstream fs;
-
-            const string& wavFile = "..\\..\\..\\cppSampleTests\\CognitiveServicesVoiceAssistantIntro.wav";
-
-            fs.open(wavFile, ios_base::binary | ios_base::in);
-
-            if ((fs.rdstate() & fs.failbit) != 0)
-            {
-                Assert::Fail();
-            }
-
-            fs.seekg(44);
-
-            std::array<uint8_t, 1000> buffer;
-
             AudioPlayer::WindowsAudioPlayer player;
             player.Initialize();
 
-            while (!fs.eof())
-            {
-                fs.read((char*)buffer.data(), (uint32_t)buffer.size());
-                result = player.Play(buffer.data(), buffer.size());
-                bytesRead += 1000;
-            }
-
-            fs.close();
-
-            SleepDuration(bytesRead);
+            int result = ReadWavFileAndInitializeWindowsAudioPlayer(player);
 
             Assert::AreEqual(rc, result);
         }
 
         TEST_METHOD(TestWindowsAudioPlayerPullAudioOutputStream) 
         {
+            int rc = 0;
+            int result = 1;
+            unsigned int bytesRead = 0;
+
             std::shared_ptr<fstream> fs = std::make_shared<fstream>();
 
             const string& wavFile = "..\\..\\..\\cppSampleTests\\CognitiveServicesVoiceAssistantIntro.wav";
@@ -75,10 +53,6 @@ namespace cppSampleTests
 
             fs->seekg(44);
 
-            int rc = 0;
-            int result = 1;
-            unsigned int bytesRead = 0;
-
             AudioPlayer::WindowsAudioPlayer player;
             player.Initialize();
 
@@ -91,6 +65,22 @@ namespace cppSampleTests
         TEST_METHOD(TestWindowsAudioPlayerStop) 
         {
             int rc = 0;
+            AudioPlayer::WindowsAudioPlayer player;
+            player.Initialize();
+
+            int result = ReadWavFileAndInitializeWindowsAudioPlayer(player);
+
+            result = player.Stop();
+            Assert::AreEqual(rc, result);
+        }
+
+        void SleepDuration(int numBytes) 
+        {
+            Sleep((numBytes / 32000) * 1000);
+        }
+
+        int ReadWavFileAndInitializeWindowsAudioPlayer(AudioPlayer::WindowsAudioPlayer& player)
+        {
             int bytesRead = 0;
             int result = 1;
             fstream fs;
@@ -108,9 +98,6 @@ namespace cppSampleTests
 
             std::array<uint8_t, 1000> buffer;
 
-            AudioPlayer::WindowsAudioPlayer player;
-            player.Initialize();
-
             while (!fs.eof())
             {
                 fs.read((char*)buffer.data(), (uint32_t)buffer.size());
@@ -122,16 +109,7 @@ namespace cppSampleTests
 
             SleepDuration(bytesRead);
 
-            result = player.Stop();
-            Assert::AreEqual(rc, result);
+            return result;
         }
-
-        void SleepDuration(int numBytes) 
-        {
-            Sleep((numBytes / 32000) * 1000);
-        }
-
-    private:
-        fstream fs;
     };
 }
