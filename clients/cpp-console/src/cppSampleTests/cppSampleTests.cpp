45d838ad10a941563a3d3dc52bd19355b7ab3640
Sajjad Plumber (INFOSYS LIMITED) <v-saplum@microsoft.com>
2020-06-18 08:31:28 -0700

    remove pch.h and pch.cpp from cppCore and cppSampleTests

--- clients/cpp-console/src/cppSampleTests/cppSampleTests.cpp
+++ clients/cpp-console/src/cppSampleTests/cppSampleTests.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#include "pch.h"
 #include "CppUnitTest.h"
 #include "WindowsAudioPlayer.h"
 #include "AudioPlayerStreamImpl.h"
