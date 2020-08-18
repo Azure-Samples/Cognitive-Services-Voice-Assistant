45b283790c90ce7faa12cc7a9e5e301ce16bfe10
Christopher Schraer <32145632+chschrae@users.noreply.github.com>
2020-08-18 09:27:36 -0700

    Adding the Voice Assistant generator project (#513)
    * first checkin for test generator
    
    * handled all stylecop warnings
    
    * fixed last property not being written
    
    * fixed bug about last property in {} block. Removed Resource manager
    
    * Adding readme
    
    * removed extra folder and fixed small issues found in PR
    
    * removed unnecessary xml
    
    * removed test generator code from test program.cs
    
    * replaced missing brace

---
+++ clients/csharp-dotnet-core/voice-assistant-test-generator/Program.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace VoiceAssistantTestGenerator
+{
+    /// <summary>
+    /// Main Program class that contains the entry point.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CommandLineArgs commandLineArgs = new CommandLineArgs(args);
+
+            if (string.IsNullOrEmpty(commandLineArgs.TSVFile))
+            {
+                commandLineArgs.PrintUsage();
+                return;
+            }
+
+            RunGeneration(commandLineArgs);
+        }
+
+        private static void RunGeneration(CommandLineArgs commandLineArgs)
+        {
+            if (commandLineArgs.TSVFile != null)
+            {
+                TestFileGenerator.GenerateTestFile(commandLineArgs.TSVFile, commandLineArgs.OutputFile);
+            }
+        }
+    }
+}
