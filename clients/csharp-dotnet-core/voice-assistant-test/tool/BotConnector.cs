41b5bb541da5e3a57c4eac1fce3b93b8618d3b03
Olivier Midy <olmidy@microsoft.com>
2020-11-05 14:59:42 -0800

    Remove whitespace.

--- clients/csharp-dotnet-core/voice-assistant-test/tool/BotConnector.cs
+++ clients/csharp-dotnet-core/voice-assistant-test/tool/BotConnector.cs
@@ -113,7 +113,6 @@ namespace VoiceAssistantTest
                 // If a custom speech endpoint is not specified, keyword verification is set
                 // according to the default or configured settings.
                 config.SetProperty("KeywordConfig_EnableKeywordVerification", this.appsettings.UseKeywordVerification.ToString().ToLower());
-
             }
 
             if (!string.IsNullOrWhiteSpace(this.appsettings.CustomVoiceDeploymentIds))
