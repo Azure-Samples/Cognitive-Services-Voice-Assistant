ced4ebd26b968fb934bbca97a1d5f63c3dbe3bcf
Chris Schraer <chris_schraer@noreply.com>
2020-11-10 16:17:42 -0800

    added DialogData.cs

---
+++ clients/csharp-dotnet-core/voice-assistant-test-generator/DialogData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoiceAssistantTestGenerator
+{
+    public class DialogData
+    {
+        public string Description = string.Empty;
+        public bool Skip = false;
+        public int TurnID = 0;
+        public int Sleep = 0;
+        public string WavFile = string.Empty;
+        public string Utterance = string.Empty;
+        public string Activity = string.Empty;
+        public bool Keyword = false;
+    }
+}
