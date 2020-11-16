c625af43151db5381c8799c19d55e9875f07b877
Chris Schraer <chris_schraer@noreply.com>
2020-11-16 11:03:15 -0800

    refactored dialogData to eliminate warnings.

--- clients/csharp-dotnet-core/voice-assistant-test-generator/DialogData.cs
+++ clients/csharp-dotnet-core/voice-assistant-test-generator/DialogData.cs
@@ -1,18 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="DialogData.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace VoiceAssistantTestGenerator
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This class stores data associated with a Dialog.
+    /// </summary>
     public class DialogData
     {
-        public string Description = string.Empty;
-        public bool Skip = false;
-        public int TurnID = 0;
-        public int Sleep = 0;
-        public string WavFile = string.Empty;
-        public string Utterance = string.Empty;
-        public string Activity = string.Empty;
-        public bool Keyword = false;
+        private string description = string.Empty;
+        private bool skip = false;
+        private int turnID = 0;
+        private int sleep = 0;
+        private string wavFile = string.Empty;
+        private string utterance = string.Empty;
+        private string activity = string.Empty;
+        private bool keyword = false;
+
+        /// <summary>
+        /// Gets or sets The Description of the dialog.
+        /// </summary>
+        public string Description { get => this.description; set => this.description = value; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the dialog should be skipped.
+        /// </summary>
+        public bool Skip { get => this.skip; set => this.skip = value; }
+
+        /// <summary>
+        /// Gets or sets The TurnID of the dialog.
+        /// </summary>
+        public int TurnID { get => this.turnID; set => this.turnID = value; }
+
+        /// <summary>
+        /// Gets or sets a value indicating how long to sleep before beginning the Dialog.
+        /// </summary>
+        public int Sleep { get => this.sleep; set => this.sleep = value; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the WavFile to be read.
+        /// </summary>
+        public string WavFile { get => this.wavFile; set => this.wavFile = value; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the utterance to use as a message to the service.
+        /// </summary>
+        public string Utterance { get => this.utterance; set => this.utterance = value; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the activity to send to the service.
+        /// </summary>
+        public string Activity { get => this.activity; set => this.activity = value; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether we should use keyword listening or not.
+        /// </summary>
+        public bool Keyword { get => this.keyword; set => this.keyword = value; }
     }
 }
