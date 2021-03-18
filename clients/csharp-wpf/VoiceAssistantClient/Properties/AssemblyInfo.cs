d0742267acf7c3efb3208292f814dbb08809979c
Darren <39422044+dargilco@users.noreply.github.com>
2021-03-18 07:58:09 -0700

    Use Speech SDK version 1.16

--- clients/csharp-wpf/VoiceAssistantClient/Properties/AssemblyInfo.cs
+++ clients/csharp-wpf/VoiceAssistantClient/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.15.0.0")]
+[assembly: AssemblyVersion("1.16.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
